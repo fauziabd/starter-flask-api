08f0bd17b2c88e0933369b3d613d6f58def98016
Fauzi <13282094+fauziabd@users.noreply.github.com>
2024-01-01 04:56:44 +0700

    .

--- app.py
+++ app.py
@@ -1,7 +1,44 @@
-from flask import Flask
+# Import the packages
+from flask import Flask, request, send_file, render_template, url_for
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from io import BytesIO
+import urllib.parse
+import time
 import os
 
+# Create a Flask app
 app = Flask(__name__)
+
+
+# Define a route for taking screenshots
+@app.route("/screenshot")
+def screenshot():
+  # Get the query parameters
+  url = request.args.get("url") or 'https://google.com'
+  width = int(request.args.get("width") or 1200)
+  height = int(request.args.get("height") or 630)
+
+  # Validate the parameters
+  if not url:
+    return "Invalid parameters"
+
+  # Create a webdriver instance with headless option
+  chrome_options = Options()
+  chrome_options.add_argument('--headless')
+  chrome_options.add_argument('--no-sandbox')
+  chrome_options.add_argument('--disable-dev-shm-usage')
+
+  driver = webdriver.Chrome(options=chrome_options)
+
+  driver.set_window_size(width, height)
+
+  # Navigate to the URL
+  driver.get(urllib.parse.unquote(url))
+  time.sleep(2)
+  screenshot = driver.get_screenshot_as_png()
+  driver.quit()
+  return send_file(BytesIO(screenshot), mimetype='image/png')
 
 @app.route('/')
 def hello_world():
